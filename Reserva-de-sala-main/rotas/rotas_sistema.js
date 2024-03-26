32628b9c5280e5f8bcf802dfea5f514c439c3459
Rennan Alves <rennandev7@gmail.com>
2024-03-26 10:33:19 -0300

    login funcionando

--- Reserva-de-sala-main/rotas/rotas_sistema.js
+++ Reserva-de-sala-main/rotas/rotas_sistema.js
@@ -4,8 +4,19 @@ const express = require('express');
 // Extraindo a função Router do módulo express
 const router = express.Router();
 
+//importar o módulo para armanezar a sessão de login
+const session = require('express-session');
+
+// Configuração do middleware de sessão
+router.use(session({
+    secret: 'pega_por_favor',
+    resave: false,
+    saveUninitialized: true
+}));
+
 // Importar módulo de serviços
 const servico = require('../servicos/servicos_sistema');
+const conexao = require('../bd/conexao_mysql');
 
 //rota principal
 router.get('/adminLogin', function(req, res){
@@ -14,31 +25,37 @@ router.get('/adminLogin', function(req, res){
 
 // Rota para lidar com a submissão do formulário de login
 router.post('/login', (req, res) => {
-    console.log("nao");
-    const email = req.body.email;
-    console.log(email);
-    const senha = req.body.senha;
-    console.log(senha);
-    console.log("nao");
-  
-    servico.loginAdmin(email, senha, (err, results) => {
-      if (err) {
-        res.status(500).send('Erro interno do servidor');
-        return;
-      }
-  
-      if (results.length > 0) {
-        // Se as credenciais estiverem corretas, redirecione para a página de reservas
-        console.log("nao");
-        res.redirect('/reservasAdm');
-      } else {
-        // Se as credenciais estiverem incorretas, exiba uma mensagem de erro
-        console.log("nao");
-        res.send('Credenciais inválidas');
-      }
-    });
-  });
+  console.log("nao");
+  const email = req.body.email;
+  console.log(email);
+  const senha = req.body.senha;
+  console.log(senha);
+  console.log("nao");
 
+  if (!email || !senha) {
+      return res.render('adminLogin', { erro: 'Por favor, preencha todos os campos.' });
+  }
+
+  let sql = `SELECT * FROM admin WHERE email = ? AND senha = ?`;
+  conexao.query(sql, [email, senha], function(err, result) {
+      console.log(result);
+      if (err) {
+          return res.render('login', { erro: 'Erro ao consultar o banco de dados' });
+      }
+      if (result.length > 0) {
+          console.log("Entrou no servicos_sistema.js");
+          req.session.usuario = result[0]; // Definindo o usuário na sessão
+          res.redirect('/reservasAdm'); // Redireciona para a página de gerente se as credenciais estiverem corretas
+      } else {
+          return res.render('adminLogin', { erro: 'Credenciais Inválidas' });
+      }
+  });
+});
+
+
+router.get('/reservasAdm', function(req,res) {
+  servico.reservasAdm(req,res);
+})
 
 
   //rota de login do gerente de TI (mova esta rota para cima)
--- Reserva-de-sala-main/servicos/servicos_sistema.js
+++ Reserva-de-sala-main/servicos/servicos_sistema.js
@@ -15,18 +15,23 @@ function adminLogin(req,res) {
     res.render('adminLogin');
 }
 
-function loginAdmin(email, senha, err) {
+function reservasAdm(req,res) {
+    res.render('reservasAdm');
+}
 
+function loginAdmin(req, res, email, senha, err) {
     if (!email || !senha) {
         return res.render('adminLogin', { erro: 'Por favor, preencha todos os campos.' });
     }
 
     let sql = `SELECT * FROM admin WHERE email = ? AND senha = ?`;
     conexao.query(sql, [email, senha], function(err, result) {
+        console.log(result)
         if (err) {
             return res.render('login', { erro: 'Erro ao consultar o banco de dados' });
         }
         if (result.length > 0) {
+            console.log("Entrou no servicos_sistema.js");
             req.session.usuario = result[0]; // Definindo o usuário na sessão
             res.redirect('/reservasAdm'); // Redireciona para a página de gerente se as credenciais estiverem corretas
         } else {
@@ -35,9 +40,11 @@ function loginAdmin(email, senha, err) {
     });
 }
 
+
 // Exportar funções
 module.exports = {
     paginaPrincipal,
     adminLogin,
-    loginAdmin
+    loginAdmin,
+    reservasAdm
 };
